6eb594173422cb7832206882686a3e00e5dc3f21
Chao Zhang <cazhang@linkedin.com>
2020-10-28 17:23:12 -0700

    Test Detekt Sarif

---
+++ src/main/kotlin/A.kt
@@ -0,0 +1,13 @@
+class A {
+    fun longLinesAreBadAndShortLinesAreGoodAndIsThisLineGoodOrBadLongLinesAreBadAndShortLinesAreGoodAndIsThisLineGoodOrBad(value: Int): Int {
+        return value * value
+    }
+
+
+    fun foo(bar: Int): Int {
+        if (bar > 5) {
+
+        }
+        return bar * bar
+    }
+}
